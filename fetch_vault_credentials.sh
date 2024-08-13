50ee5a3d5e57c00f03ee2fab283a8b1702db7ba6
Dhruv Gogri <100945297+Dhruvgogri18@users.noreply.github.com>
2024-08-13 11:17:20 +0530

    Update fetch_vault_credentials.sh

--- fetch_vault_credentials.sh
+++ fetch_vault_credentials.sh
@@ -2,7 +2,7 @@
 export VAULT_ADDR='http://192.168.1.51:8200'
 export VAULT_TOKEN='test'
 # Fetch credentials from Vault
-USERNAME=$(vault kv get -field=username secret/data/myapp)
+USERNAME=$(vault kv get -field=email secret/data/myapp)
 PASSWORD=$(vault kv get -field=password secret/data/myapp)
 # Export these variables so they can be used in Concourse tasks
 export VAULT_USERNAME=$USERNAME
